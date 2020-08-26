9c57d6c5317d346a3100893cc4fd101f77456e17
MohammadAgung <66293999+mohammad-agung@users.noreply.github.com>
2020-08-26 08:26:22 +0800

    user view and aside-user

---
+++ page/user/user-view.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+if ($_SESSION['level'] == 'admin') {
+    header('Location: ../../page/admin/dashboard');
+    exit;
+} else if ($_SESSION['level'] == 'user') {
+    include '../../controller/config.php';
+    include '../../view/header.php';
+?>
+    <title>Dashboard User</title>
+    </head>
+
+    <body class="animsition">
+        <div class="page-wrapper">
+            <!-- HEADER MOBILE-->
+            <?php include '../../view/header-mobile-user.php'; ?>
+            <!-- END HEADER MOBILE-->
+
+            <!-- MENU SIDEBAR-->
+            <?php include '../../view/aside-user.php'; ?>
+            <!-- END MENU SIDEBAR-->
+
+            <!-- PAGE CONTAINER-->
+            <div class="page-container">
+                <!-- HEADER DESKTOP-->
+                <?php include '../../view/header-desktop.php'; ?>
+                <!-- HEADER DESKTOP-->
+
+                <!-- MAIN CONTENT-->
+                <div class="main-content">
+                    <div class="section__content section__content--p30">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <!-- DATA TABLE-->
+                                    <div class="table-responsive m-b-40">
+                                        <table class="table table-borderless table-data3">
+                                            <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>Nama User</th>
+                                                    <th>Username</th>
+                                                    <th>Status</th>
+                                                    <th>Kesatuan</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                $kesatuan = $_SESSION['kesatuan'];
+                                                $query = mysqli_query($conn, "SELECT * from tbl_user where nama_kesatuan='$kesatuan' ORDER BY status");
+                                                $row_count = mysqli_num_rows($query);
+                                                $number = 1;
+                                                if ($row_count == 0) {
+                                                ?>
+                                                    <tr>
+                                                        <td colspan="5">
+                                                            <h3 style="color:red;text-align:center">No record found</h3>
+                                                        </td>
+                                                    </tr>
+                                                    <?php
+                                                } else {
+                                                    while ($row = mysqli_fetch_array($query)) {
+                                                    ?>
+                                                        <tr>
+                                                            <td><?= $number++; ?></td>
+                                                            <td><?= htmlspecialchars($row['nama_user']); ?></td>
+                                                            <td><?= htmlentities($row['username']); ?></td>
+                                                            <?php if ($rowUser['status'] == 1) { ?>
+                                                                <td class="process">Active</td>
+                                                            <?php } else if ($rowUser['status'] == 2) { ?>
+                                                                <td class="denied">Not Actived</td>
+                                                            <?php } ?>
+                                                            <td><?= $kesatuan; ?></td>
+                                                        </tr>
+                                                    <?php } ?>
+                                                <?php } ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <!-- END DATA TABLE-->
+                                </div>
+                            </div>
+                            <?php include '../../view/copyright.php'; ?>
+                        </div>
+                    </div>
+                </div>
+                <!-- END MAIN CONTENT-->
+                <!-- END PAGE CONTAINER-->
+            </div>
+
+        </div>
+    <?php
+    include '../../view/footer.php';
+} else {
+    header('Location: ../');
+} ?>
--- view/aside-user.php
+++ view/aside-user.php
@@ -28,10 +28,10 @@
                         <i class="fas fa-database"></i>Jenis Pelanggaran R2/R4</a>
                     <ul class="list-unstyled navbar__sub-list js-sub-list">
                         <li>
-                            <a href="login.html">Tambah Data</a>
+                            <a href="#">Tambah Data</a>
                         </li>
                         <li>
-                            <a href="register.html">Lihat Data</a>
+                            <a href="#">Lihat Data</a>
                         </li>
                     </ul>
                 </li>
@@ -40,10 +40,10 @@
                         <i class="fas fa-chart-bar"></i>Data Pelanggaran</a>
                     <ul class="list-unstyled navbar__sub-list js-sub-list">
                         <li>
-                            <a href="login.html">Laporan Tabel</a>
+                            <a href="#">Laporan Tabel</a>
                         </li>
                         <li>
-                            <a href="register.html">Laporan Grafik</a>
+                            <a href="#">Laporan Grafik</a>
                         </li>
                     </ul>
                 </li>
@@ -52,10 +52,7 @@
                         <i class="fas fa-user"></i>User</a>
                     <ul class="list-unstyled navbar__sub-list js-sub-list">
                         <li>
-                            <a href="login.html">Tambah User</a>
-                        </li>
-                        <li>
-                            <a href="register.html">Lihat User</a>
+                            <a href="user-view">Lihat User</a>
                         </li>
                     </ul>
                 </li>
