532a7e9aea70f4f567853804f6c5a02f8b97d347
MohammadAgung <66293999+mohammad-agung@users.noreply.github.com>
2020-08-31 00:20:53 +0800

    ubah template login

--- assets/login/js/main.js
+++ assets/login/js/main.js
@@ -2,21 +2,6 @@
 (function ($) {
     "use strict";
 
-
-    /*==================================================================
-    [ Focus Contact2 ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
-                $(this).addClass('has-val');
-            }
-            else {
-                $(this).removeClass('has-val');
-            }
-        })    
-    })
-  
-  
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
@@ -65,6 +50,7 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
+    
     
 
 })(jQuery);
